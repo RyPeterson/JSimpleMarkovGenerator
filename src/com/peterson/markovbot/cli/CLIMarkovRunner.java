eeffa570f2a1441356d0cbc8c80c364bbf0314a0
sadjava <r.peterson104@gmail.com>
2015-03-21 13:49:07 -0700

    created main class to run a Markov Bot on the command line

---
+++ src/com/peterson/markovbot/cli/CLIMarkovRunner.java
@@ -0,0 +1,17 @@
+package com.peterson.markovbot.cli;
+
+/**
+ * A Command-Line Interface to a MarkovBot.
+ * This is a main class that runs a MarkovBot on the command line,
+ * receiving input from a user.
+ * @author Peterson, Ryan
+ *         Created 3/21/2015
+ */
+public class CLIMarkovRunner
+{
+
+    public static void main(String []args)
+    {
+
+    }
+}
