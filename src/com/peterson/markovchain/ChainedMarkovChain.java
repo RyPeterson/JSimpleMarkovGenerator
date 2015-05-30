5ce7c25dab4285829cb3f8d537be52e6ca169900
sadjava <r.peterson104@gmail.com>
2015-05-30 15:52:51 -0700

    created ChainedMarkovChain that implements MarkovChain, and auto-generated appropriate methods from IDE

---
+++ src/com/peterson/markovchain/ChainedMarkovChain.java
@@ -0,0 +1,27 @@
+package com.peterson.markovchain;
+
+/**
+ * @author Peterson, Ryan
+ *         Created: 5/30/15
+ */
+public class ChainedMarkovChain implements MarkovChain
+{
+
+    @Override
+    public void addPhrase(String phrase)
+    {
+
+    }
+
+    @Override
+    public String generateSentence()
+    {
+        return null;
+    }
+
+    @Override
+    public String generateSentence(String seed)
+    {
+        return null;
+    }
+}
