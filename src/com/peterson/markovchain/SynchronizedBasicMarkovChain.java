bc0d3486ec125f5c0e71dcb31b1493a4c3ec033e
sadjava <r.peterson104@gmail.com>
2015-06-08 14:42:34 -0700

    added SynchronizedBasicMarkovChain and adjused a internal method to properly set object up for concurrent access

---
+++ src/com/peterson/markovchain/SynchronizedBasicMarkovChain.java
@@ -0,0 +1,41 @@
+package com.peterson.markovchain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Extension of the BasicMarkovChain to provide synchronized access to the generator.
+ * This overrides the basic structure of the generator to use a synchronized map and lists.
+ * @author Peterson, Ryan
+ *         Created: 6/8/15
+ */
+public class SynchronizedBasicMarkovChain extends BasicMarkovChain
+{
+    /**
+     * Constructs a synchronized version of the generator.
+     * This provides all the basic functionality to generate
+     * markov chains, as described in the parent class.
+     */
+    public SynchronizedBasicMarkovChain()
+    {
+        super(true);
+    }
+
+    /**
+     * Constructs a synchronized version of the generator using an initial set of phrases
+     * @param phrases
+     */
+    public SynchronizedBasicMarkovChain(String... phrases)
+    {
+        super(true);
+        for(String s : phrases)
+            super.addPhrase(s);
+    }
+
+    @Override
+    protected List<String> newList()
+    {
+        return Collections.synchronizedList(new ArrayList<>());
+    }
+}
