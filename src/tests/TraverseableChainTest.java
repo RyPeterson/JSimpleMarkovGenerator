eef0b68ef71b1dee2178164dff1116a180601bb1
sadjava <r.peterson104@gmail.com>
2015-06-09 15:46:58 -0700

    added test to the traversable markov chain so that nulls cannot be used

--- src/tests/TraverseableChainTest.java
+++ src/tests/TraverseableChainTest.java
@@ -1,9 +1,18 @@
 package tests;
 
+import com.peterson.markovchain.TraversableMarkovChain;
+import org.junit.Before;
+
 /**
  * @author Peterson, Ryan
  *         Created: 6/9/15
  */
-public class TraverseableChainTest
+public class TraverseableChainTest extends BasicMarkovTest
 {
+    @Before
+    @Override
+    public void setUp()
+    {
+        markovChain = new TraversableMarkovChain();
+    }
 }
